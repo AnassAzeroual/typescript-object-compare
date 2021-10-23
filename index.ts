5279e5f84d022b6ad6059511da663942b9c34afe
AnassAzeroual <azeroual.anass2015@gmail.com>
2021-10-24 01:12:46 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-const person1 = {first_name:'anass',last_name:'azeroual'}; // Object 1
-const person2 = {first_name:'anass',last_name:'azeroual'}; // Object 2
+const person1 = { first_name: 'anass', last_name: 'azeroual' }; // Object 1
+const person2 = { first_name: 'anass', last_name: 'azeroual' }; // Object 2
 
-function sortObjectByKeys(o) { // Function sort by Keys
-  return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
+function sortObjectByKeys(o) {
+  // Function sort by Keys
+  return Object.keys(o)
+    .sort()
+    .reduce((r, k) => ((r[k] = o[k]), r), {});
 }
 
-let isEqual = JSON.stringify(sortObjectByKeys(person1)) === JSON.stringify(sortObjectByKeys(person2));
+let isEqual =
+  JSON.stringify(sortObjectByKeys(person1)) ===
+  JSON.stringify(sortObjectByKeys(person2));
 
-console.log(isEqual)
+console.log(isEqual); // Open the console!
